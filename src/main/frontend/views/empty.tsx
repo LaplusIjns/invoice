d8f8999f1d55c49b06aac56048265751d0d1e2a8
start.vaadin.com <no-reply@vaadin.com>
2026-01-31 07:33:29 +0000

    Generated project

---
+++ src/main/frontend/views/empty.tsx
@@ -0,0 +1,16 @@
+import { ViewConfig } from '@vaadin/hilla-file-router/types.js';
+
+export const config: ViewConfig = {
+  menu: { order: 1, icon: 'line-awesome/svg/money-bill-solid.svg' },
+  title: 'result',
+};
+
+export default function ResultView() {
+  return (
+    <div className="flex flex-col h-full items-center justify-center p-l text-center box-border">
+      <img style={{ width: '200px' }} src="images/empty-plant.png" />
+      <h2>This place intentionally left empty</h2>
+      <p>It’s a place where you can grow your own UI 🤗</p>
+    </div>
+  );
+}
